0efad610d23280c82b676cea03e44daeabf0d7b4
Ashish Nandekar <ashunandekar35@gmail.com>
2026-06-21 12:04:50 +0530

    Refactor dynamic array implementation and comments

--- Data Structures & Algorithms/dynamicArray/submission-0.java
+++ Data Structures & Algorithms/dynamicArray/submission-0.java
@@ -2,20 +2,21 @@ class DynamicArray {
     private int[] arr;
     private int capacity;
     private int size;
+    //initialize empty array
     public DynamicArray(int capacity) {
         this.capacity = capacity;
         this.size = 0;
         this.arr = new int[this.capacity];
     }
-
+    //return i-th element
     public int get(int i) {
         return arr[i];
     }
-
+    //insert element n at i-th position
     public void set(int i, int n) {
         arr[i] = n;
     }
-
+    //insert at end and double the array if size == to capacity
     public void pushback(int n) {
         if (size == capacity) {
             resize();
@@ -23,16 +24,16 @@ class DynamicArray {
         arr[size] = n;
         size += 1;
     }
-
+    //soft delete the last element
     public int popback() {
         if(size > 0){
         size -= 1;
         }
         return arr[size];
     }
-
+    //double the size of array
     public void resize() {
-        capacity = 2 * capacity;
+        capacity *= 2;
         int[] new_arr = new int[capacity];
 
         for (int i = 0; i < size; i++) {
@@ -41,11 +42,11 @@ class DynamicArray {
 
         arr = new_arr;
     }
-
+    //return the current present element
     public int getSize() {
         return size;
     }
-
+    //return the actual capacity of array
     public int getCapacity() {
         return capacity;
     }
